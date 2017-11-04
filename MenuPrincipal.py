91a3e8fd7f59e27adc91787038e4dc98a95ab940
Victor <vprlisboa@gmail.com>
2017-11-04 18:31:30 -0200

    Adicionando botão para o método da bisseção;adicionando atualizando a ajuda para contemplar os métodos existentes

--- MenuPrincipal.py
+++ MenuPrincipal.py
@@ -1,5 +1,4 @@
 from tkinter import *
-import sys
 import os
 class menuTrabalhoCG:
     def __init__(self, master):
@@ -13,7 +12,7 @@ class menuTrabalhoCG:
         self.label.grid(row=0, column=1)
 
         #frame para os poligonos
-        self.poligonos = Frame(master)
+        self.frame = Frame(master)
 
         #botao que fecha o menu
         self.close_button = Button(master, text="Fechar", command=master.quit)
@@ -25,19 +24,20 @@ class menuTrabalhoCG:
         self.master.config(menu=self.principal)
         self.principal.add_command(label="Ajuda", command=self.Ajuda)
 
-        #variavel que salva a textura escolhida
-        self.textura = StringVar(master)
+        #botao que seleciona o método de newton
+        self.executaMetodoDeNewton = Button(master, text="Método de Newton", command=self.MetodoDeNewton)
+        self.executaMetodoDeNewton.config(height=2, width=15)
+        self.executaMetodoDeNewton.grid(row=1, column=1)
 
-        #botao que seleciona o cubo
-        self.botao_cubo = Button(master, text="Método de Newton", command=self.Cubo)
-        self.botao_cubo.config(height=2, width=15)
-        self.botao_cubo.grid(row=1, column=1)
+        # botao que seleciona o método da bisseção
+        self.executaBissecao = Button(master, text="Método da Bisseção", command=self.MetodoBissecao)
+        self.executaBissecao.config(height=2, width=15)
+        self.executaBissecao.grid(row=2, column=1)
 
     def Ajuda(self):
-        texto_ajuda = 'Para usar o programa, primeiro escolha um método numérico\n' \
-                      ' Os operadores são:                                       \n'\
-                      '  - \'*\' : Multiplicação                                  \n'\
-                      '  - \'**\' : Potenciação                                   \n' \
+        texto_ajuda = 'Os métodos disponíveis são:                              \n' \
+                      ' - Método de Newton                                       \n'\
+                      ' - Método da Bisseção                                     \n'\
                       '  - \'+\' : Adição                                         \n' \
                       '  - \'-\' : Subtração                                      \n' \
                       '  - \'/\' : Divisão                                         \n' \
@@ -46,8 +46,11 @@ class menuTrabalhoCG:
         self.label = Label(self.pop_up, text = texto_ajuda, height=12, width=60,font=("Fixedsys",12))
         self.label.pack(expand=True)
 
-    def Cubo(self):
+    def MetodoDeNewton(self):
         os.system('python3 MetodoDeNewton.py')
+
+    def MetodoBissecao(self):
+        os.system('python3 MetodoBissecao.py')
 
 principal = Tk()
 menu = menuTrabalhoCG(principal)
